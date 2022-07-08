c619dbdb29072d98674e37f2f3d04f56188b84ca
snoop2head <young_ahn@yonsei.ac.kr>
2022-07-09 07:27:41 +0900

    translate cc12m sample with all trained t5 base model

--- inference.py
+++ inference.py
@@ -7,15 +7,21 @@ with open("config.yaml") as infile:
     SAVED_CFG = yaml.load(infile, Loader=yaml.FullLoader)
     CFG = EasyDict(SAVED_CFG["CFG"])
 
-src_text = ["My name is Sarah and I live in London"]
+# https://huggingface.co/datasets/conceptual_captions
+src_text = [
+    "sierra looked stunning in this top and this skirt while performing with person at their former university"
+]
 
-model_name = "/home/ubuntu/En_to_Ko/ke-t5-base-finetuned-en-to-ko/checkpoint-1650"
+model_name = "/home/ubuntu/En_to_Ko/ke-t5-base-finetuned-en-to-ko/checkpoint-17850"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 
 translated = model.generate(
     **tokenizer(src_text, return_tensors="pt", padding=True, max_length=CFG.max_token_length,),
     max_length=CFG.max_token_length,
-    no_repeat_ngram_size=1,
+    num_beams=5,
+    repetition_penalty=1.3,
+    no_repeat_ngram_size=3,
+    num_return_sequences=3,
 )
 print([tokenizer.decode(t, skip_special_tokens=True) for t in translated])
